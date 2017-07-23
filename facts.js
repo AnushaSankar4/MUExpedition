61c5f5be02b4fa84902ca6652cb66baa33beba47
Anusha Shankar <anusha.shankar4@gmail.com>
2017-07-23 20:52:42 +0530

    updated facts

--- facts.js
+++ facts.js
@@ -2,36 +2,40 @@
 
 module.exports = {
     "trophydata" : [
-			"Manchester United have won a record 13 titles in the Premier League since the top flight of English football was given that new name in 1992.Sir Alex Ferguson was the man who masterminded the triumph in the inaugural Premier League season, in 1992/93. The Reds have since dominated the league by winning titles in 1994, 1996, 1997, 1999, 2000, 2001, 2003, 2007, 2008, 2009, 2011 and 2013.",
-			"Manchester United have won seven First Division Championships before 1992. The club's first League Championship was secured in 1908, six years after the name of Newton Heath - league entrants in 1892 - was changed to Manchester United. A second title followed in 1911 but it wasn’t until after the Second World War, when United really became a force in English football under the leadership of Sir Matt Busby.",
-			"Manchester United have 21 Community Shield successes in total on the honours board - 17 outright triumphs and four shared after a draw. Introduced as the FA Charity Shield in 1908, it had evolved from the Sheriff of London Shield match, which was played between a top professional side and a leading amateur team. This format was maintained when United, the reigning Football League champions, beat Queens Park Rangers, the then-Southern League champions, 4-0 in a replay after the first match had ended 1-1. Both games were at Stamford Bridge.",
-			"Also known as the World Club or Toyota Cup, the Inter-Continental Cup was the brainchild of Henri Delauney, the former general secretary of UEFA. His suggestion, that the champions of two continents should compete for one global trophy, also inspired the creation of the Copa Libertadores, South America’s answer to the European Cup. Between 1960 and 1980 all finals were played over two legs. Strangely up until 1968 points, not aggregate score, was the decisive factor and if the points were level, a third match was required. The first winners of the competition were Real Madrid; AC Milan, Penarol and Nacional have since won the competition a record three times each.",
-			"Manchester United became the first British club to win the FIFA Club World Cup when the Reds beat LDU Quito in Yokohama, Japan on 21 December 2008. Wayne Rooney scored the only goal. The 1-0 win over LDU Quito in the final was the second of two matches United played in the tournament - Sir Alex Ferguson's men also won a semi-final a few days earlier against Gamba Osaka, beating the Japanese side 5-3 with goals from Rooney (2), Nemanja Vidic, Cristiano Ronaldo and Darren Fletcher.",
+			"Manchester United have won a record 13 titles in the Premier League since the top flight of English football was given that new name in 1992.Sir Alex Ferguson was the man who masterminded the triumph in the inaugural Premier League season, in 1992/93. The Reds have since dominated the league by winning titles in 1994, 1996 and a few more. ",
+			"Manchester United have won seven First Division Championships before 1992. The club's first League Championship was secured in 1908, six years after the name of Newton Heath - league entrants in 1892 - was changed to Manchester United. A second title followed in 1911 but it wasn’t until after the Second World War, when United really became a force in English football under the leadership of Sir Matt Busby. ",
+			"Manchester United have 21 Community Shield successes in total on the honours board - 17 outright triumphs and four shared after a draw. Introduced as the FA Charity Shield in 1908, it had evolved from the Sheriff of London Shield match, which was played between a top professional side and a leading amateur team. This format was maintained when United, the reigning Football League champions, beat Queens Park Rangers, the then-Southern League champions, 4-0 in a replay after the first match had ended 1-1. Both games were at Stamford Bridge. ",
+			"Also known as the World Club or Toyota Cup, the Inter-Continental Cup was the brainchild of Henri Delauney, the former general secretary of UEFA. His suggestion, that the champions of two continents should compete for one global trophy, also inspired the creation of the Copa Libertadores, South America’s answer to the European Cup. Between 1960 and 1980 all finals were played over two legs. Strangely up until 1968 points, not aggregate score, was the decisive factor and if the points were level, a third match was required. The first winners of the competition were Real Madrid; AC Milan, Penarol and Nacional have since won the competition a record three times each. ",
+			"Manchester United became the first British club to win the FIFA Club World Cup when the Reds beat LDU Quito in Yokohama, Japan on 21 December 2008. Wayne Rooney scored the only goal. The 1-0 win over LDU Quito in the final was the second of two matches United played in the tournament - Sir Alex Ferguson's men also won a semi-final a few days earlier against Gamba Osaka, beating the Japanese side 5-3 with goals from Rooney (2), Nemanja Vidic, Cristiano Ronaldo and Darren Fletcher. ",
 			"Manchester United have won the FA Cup on 12 occasions - a feat bettered only by Arsenal's 13 triumphs - and have appeared in the final 19 times in total. The Reds are also the only club to appear in an FA Cup final in every decade since the war. The club's maiden FA Cup voyage was in 1890 under the name of Newton Heath but didn't progress beyond the third round until 1902, after the change of monicker to Manchester United. ",		
-			"The UEFA Champions League is the ultimate club competition in European football and Manchester United have won the trophy three times. United first played in the European Cup in 1956. The decision did not go down well with the Football League due to the fear that it would affect the quality of domestic competitions. This was quickly quashed as the competition helped raise standards in the English game."
+			"The UEFA Champions League is the ultimate club competition in European football and Manchester United have won the trophy three times. United first played in the European Cup in 1956. The decision did not go down well with the Football League due to the fear that it would affect the quality of domestic competitions. This was quickly quashed as the competition helped raise standards in the English game. "
 		],
     "stadiumdata" : [
-			"",
-			"",
-			"",
-			"",
-			""
+			"Old Trafford officially opened on the 19th of February 1910 with a match between Manchester and Liverpool (3-4). The stadium at that time consisted of one covered seating stand and open terraces on the other three sides. Capacity was slightly over 80,000.",
+			"In 1939, Old Trafford recorded its highest attendance of 76,962 during an FA Cup semi-final match between Wolves and Grimsby Town.",
+			"Due to its proximity to Trafford Park industrial estate, Old Trafford got heavily damaged by German air raids during World War 2. It took eight years to rebuilt the stadium, the delays being caused by limited post-war resources, and during that time United played at Maine Road, the ground of rivals Manchester City."
+			
+			
 			
 	],
 	"dressingroomdata" : [
-			"",
-			"",
-			"",
-			"",
-			""
+			"Redesigned in summer 2008 to replace those opened in 1993 when the dressing rooms were moved from their original location under the centre of the south stand to the south-west corner of the stadium, the home dressing room is now a modern sanctuary.",
+			"The Dressing room contains a huge flat-screen TV, three baths, six showers and a warm-up room equipped with state-of-the-art fitness facilities.",
+			"There’s  the all-important tactics board, massage tables and, of course, a music player operated by Reds DJ Patrice Evra at the jersey room.",
+			"Before the game the players' lounge can be subdued, especially if a tense fixture such as the Manchester derby awaits. Usually, though, it's a hive of activity as players mingle with friends and family and discuss the match."
+			
 			
 	],
 	"playersloungedata" : [
-			"",
-			"",
-			"",
-			"",
-			""
+			"The centre-piece of the lounge is the international honours board, displaying names of every player to have won an international cap while playing for United. It's a great reminder of the calibre of footballer to have pulled on the red shirt down the years - the latest names to be carved into the board include Tom Cleverley, Robin van Persie and Shinji Kagawa.",
+			"Before the game the players' lounge can be subdued, especially if a tense fixture such as the Manchester derby awaits. Usually, though, it's a hive of activity as players mingle with friends and family and discuss the match.",
+			" It's a modest area that has retained much of the original decor from its 1993 opening. The walls are are adorned by photographs of United Captains"
+			
+	
+	
+	],
+	"manchester" : [
+			"Old Trafford is a football stadium in Old Trafford, Greater Manchester, England, and the home of Manchester United. With a capacity of 75,643. It is the largest club football stadium in the United Kingdom, the second-largest football stadium, and the eleventh-largest in Europe."
 	
 	
 	]
