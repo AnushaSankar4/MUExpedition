a09c5e2ec070fe4daa2e4c77805e988ac2ee2b6d
Anusha Shankar <anusha.shankar4@gmail.com>
2017-07-23 13:31:45 +0530

    updated code

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 var Alexa = require('alexa-sdk');
 
-var APP_ID = "amzn1.ask.skill.60c09215-cb26-4e33-97d6-c978ed3dced7";
-
-var SKILL_NAME = "MUFacts";
-var WELCOME_MESSAGE = "Welcome to MUFacts. You can get information about the different rooms of the Old Trafford Museum ... Which room do you want information about?";
+var APP_ID = "amzn1.ask.skill.60c09215-cb26-4e33-97d6-c978ed3dced7"; //app ID as obtained from the Amazon developer's portal for the skill
+ 
+var SKILL_NAME = "MUExpedition";
+var WELCOME_MESSAGE = "Welcome to M U Expedition. You can get information about the different rooms of the Old Trafford Museum ... Which room do you want information about?";
 var WELCOME_REPROMPT = "For instructions on what you can say, please say help me.";
 var GET_FACT_MESSAGE = "Here's some information: ";
 var HELP_MESSAGE = "You can say things like, give me some information about...or mention which room you prefer to learn about...Now, what can I help you with?";
@@ -12,25 +12,27 @@ var HELP_REPROMPT = "What can I help you with?";
 var STOP_MESSAGE = "see you soon, Goodbye!";
 var CANCEL_MESSAGE= "Ok, let\'s play again soon.";
 var NO_MESSAGE= "Ok, we\'ll play another time. Goodbye!";
-var TRIVIA_UNHANDLED= "Do you need Trivia or a Clue";
+var TRIVIA_UNHANDLED= "Do you want to learn more";
 var START_UNHANDLED= "Say start over to start a new game.";
-           
+var ROOMNAME_UNHANDLED= "Sorry, I did not get you. Which room do you want information about? ";          
 
 var GAME_STATES = {
     TRIVIA: "_TRIVIAMODE", // Asking trivia questions.
     START: "_STARTMODE", // Entry point, start the game.
-	CLUE: "_CLUEMODE",
-	BRANCH: "_BRANCHMODE"
+	CLUE: "_CLUEMODE", // Giving a clue to the next spot.
+	BRANCH: "_BRANCHMODE" // User's choice:Trivia or Clue?.
 
 };
 
 var info = require("./facts");
 
+//Information on different rooms
 var facts = {
 	"trophyroom": info["trophydata"],
 	"stadium": info["stadiumdata"],
 	"artgallery": info["dressingroomdata"],
-	"jerseyroom": info["playersloungedata"]
+	"jerseyroom": info["playersloungedata"],
+	"manchester": info["manchester"]
 };
 
 exports.handler = function(event, context, callback) {
@@ -62,13 +64,10 @@ var startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
         this.emit('GetNewFactIntent');
 	}, 
 	"GetNewFactIntent": function () {
-		
-		
 		var room = this.event.request.intent.slots.roomname;
-		var factArr = undefined;
+		var factArr = facts.manchester;
 		if(room.value.toLowerCase()=="trophy room"){
 			 factArr = facts.trophyroom;
-			 //this.attributes["room"]="trophy room";
 		}
 		else if(room.value.toLowerCase()=="stadium"){
 			factArr = facts.stadium;
@@ -79,7 +78,7 @@ var startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
 		else if(room.value.toLowerCase()=="jersey room"){
 			factArr = facts.jerseyroom;
 		}
-		else{}
+		else{factArr = facts.manchester;}
 		
    
         var factIndex = Math.floor(Math.random() * factArr.length);
@@ -87,29 +86,31 @@ var startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
         //var speechOutput = GET_FACT_MESSAGE + randomFact;
 		var speechOutput="Manchester United is swag....";
 		speechOutput+="Do you want to learn more?";
-		//this.emit(':saveState',true);
+		Object.assign(this.attributes, {
+			"oldtraffroom": room.value		
+			
+        });
 		this.handler.state = GAME_STATES.BRANCH;
-        this.emit(':askWithCard', speechOutput , "Say yes if you would like to learn more." , SKILL_NAME, randomFact);
+        this.emit(':askWithCard', speechOutput , "Say yes if you would like to learn more or repeat to listen to the information again." , SKILL_NAME, randomFact);
 		
 		
-    },	
-	"AMAZON.StartOverIntent": function () {
-        this.handler.state = GAME_STATES.START;
-        this.emitWithState("StartGame", false);
+    },
+	"Unhandled": function () {
+        var speechOutput = ROOMNAME_UNHANDLED;
+        this.emit(":ask", speechOutput, speechOutput);
+    },
+	"SessionEndedRequest": function () {
+        console.log("Session ended in trivia state: " + this.event.request.reason);
     }
 });
 
-var branchStateHandlers = Alexa.CreateStateHandler(GAME_STATES.BRANCH, {
-	
-	
+var branchStateHandlers = Alexa.CreateStateHandler(GAME_STATES.BRANCH, {	
 	"AMAZON.YesIntent": function() {
-		//var room= this.attributes["room"];
-        this.handler.state = GAME_STATES.START;
-        this.emitWithState("StartGame");
-		//this.emit(':tell',"You are in "+ room);
+		//var room= this.attributes.oldtraffroom;
+		moreRoomInfo.call(this,false);
     },
 	"AMAZON.NoIntent": function() {
-        this.emit(":ask", "Would you like to play trivia or would you like a clue to the next spot?","Trivia or Clue?");
+        this.emit(":ask", "Would you like to play trivia or would you like a clue to the next spot.","Trivia or Clue?");
 		this.emit("StateIntent");
 		
     },
@@ -128,28 +129,21 @@ var branchStateHandlers = Alexa.CreateStateHandler(GAME_STATES.BRANCH, {
 		}
 		else{}
 	},
-	/*"TriviaIntent": function() {
-		this.emit(':tell',"trivia selected");
-	},
-	"ClueIntent": function() {
-		this.emit(':tell',"clue selected");
-	},*/
     "AMAZON.StartOverIntent": function () {
         this.handler.state = GAME_STATES.START;
         this.emitWithState("StartGame", false);
     },
-    /*"AMAZON.RepeatIntent": function () {
+    "AMAZON.RepeatIntent": function () {
         this.emit(":ask", this.attributes["speechOutput"], this.attributes["repromptText"]);
-    },*/
+    },
     "AMAZON.HelpIntent": function () {
         var speechOutput = HELP_MESSAGE;
         var reprompt = HELP_REPROMPT;
         this.emit(':ask', speechOutput, reprompt);
     },
     "AMAZON.StopIntent": function () {
-        this.handler.state = GAME_STATES.HELP;
         var speechOutput = STOP_MESSAGE;
-        this.emit(":ask", speechOutput, speechOutput);
+        this.emit(":tell", speechOutput);
     },
     "AMAZON.CancelIntent": function () {
         this.emit(":tell", CANCEL_MESSAGE);
@@ -282,6 +276,31 @@ var clueStateHandlers = Alexa.CreateStateHandler(GAME_STATES.CLUE, {
         console.log("Session ended in trivia state: " + this.event.request.reason);
     }
 });
+
+function moreRoomInfo() {
+		var room = this.attributes.oldtraffroom;
+		var factArr = facts.manchester;
+	
+		if(room=="trophy room"){
+			 factArr = facts.trophyroom;
+			 //this.attributes["room"]="trophy room";
+		}
+		else if(room=="stadium"){
+			factArr = facts.stadium;
+		}
+		else if(room=="art gallery"){
+			factArr = facts.artgallery;
+		}
+		else if(room=="jersey room"){
+			factArr = facts.jerseyroom;
+		}
+		else{factArr = facts.manchester;}
+		var factIndex = Math.floor(Math.random() * factArr.length);
+        var randomFact = factArr[factIndex];
+        var speechOutput = GET_FACT_MESSAGE + randomFact;
+		speechOutput+="Do you want to learn more?";
+        this.emit(':askWithCard', speechOutput , "Say yes if you would like to learn more." , SKILL_NAME, randomFact);
+}
 
 
 
